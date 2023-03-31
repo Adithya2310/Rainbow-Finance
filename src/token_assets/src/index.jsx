094fd11d3b7cca3f423fa8a8efc1a9aef00dce0f
Adithya2310 <adithyanandi099@gmail.com>
2023-03-31 17:52:46 +0530

    enable pop-ups to authenticate

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -7,21 +7,21 @@ const init = async () => {
   // checking the internet identity authentication of ICP
   const authClient = await AuthClient.create();
 
-  // if(await authClient.isAuthenticated()){
-  //   handleAuthenticated();
-  // }
-  // else{
-  //     await authClient.login({
-  //     identityProvider: "https://identity.ic0.app/#authorize",
-  //     onSucess:()=>{
-  //       handleAuthenticated();
-  //     },
-  //   });
-  // }
-  // function handleAuthenticated(){
-  //   ReactDOM.render(<App />, document.getElementById("root"));
-  // } 
-  ReactDOM.render(<App />, document.getElementById("root"));
+  // enable the pop-ups to authenticate
+  if(await authClient.isAuthenticated()){
+    handleAuthenticated();
+  }
+  else{
+      await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSucess:()=>{
+        handleAuthenticated();
+      },
+    });
+  }
+  function handleAuthenticated(){
+    ReactDOM.render(<App />, document.getElementById("root"));
+  } 
 }
 
 init();
